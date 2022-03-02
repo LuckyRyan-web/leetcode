a55b4c5cf6ea17030e4a50e733d16fb0c9426d1a
liuyuan <373005226@qq.com>
2022-03-02 14:26:49 +0800

    237

---
+++ code/linkedList/237/index.ts
@@ -0,0 +1,29 @@
+/**
+ * Definition for singly-linked list.
+ * class ListNode {
+ *     val: number
+ *     next: ListNode | null
+ *     constructor(val?: number, next?: ListNode | null) {
+ *         this.val = (val===undefined ? 0 : val)
+ *         this.next = (next===undefined ? null : next)
+ *     }
+ * }
+ */
+
+/**
+ Do not return anything, modify it in-place instead.
+ */
+
+/**
+ * 因为只可以访问被删除的节点, 所以只需要节点的 next 指向下一个节点即可
+ */
+interface ListNode {
+    val: number
+    next: ListNode | null
+}
+function deleteNode(root: ListNode | null): void {
+    if (root && root.next) {
+        root.val = root.next.val
+        root.next = root.next.next
+    }
+}
---
+++ code/queue/933/index.ts
@@ -0,0 +1,27 @@
+/**
+ * 时间复杂度为 O(N)
+ * 空间复杂度为 O(N)
+ */
+
+class RecentCounter {
+    queue: number[]
+
+    constructor() {
+        this.queue = []
+    }
+
+    ping(t: number): number {
+        this.queue.push(t)
+
+        while (this.queue[0] < t - 3000) {
+            this.queue.shift()
+        }
+        return this.queue.length
+    }
+}
+
+/**
+ * Your RecentCounter object will be instantiated and called as such:
+ * var obj = new RecentCounter()
+ * var param_1 = obj.ping(t)
+ */
