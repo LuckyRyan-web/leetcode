3fe93dd5bbf241fc6a04ae0716acbadd95eefdd0
liuyuan <373005226@qq.com>
2022-07-06 16:40:18 +0800

    添加 '118. 杨辉三角' 代码

---
+++ code/array/118/index.test.ts
@@ -0,0 +1,20 @@
+import { generate } from './index';
+
+const example = [
+    {
+        numRows: 5,
+        target: [[1], [1, 1], [1, 2, 1], [1, 3, 3, 1], [1, 4, 6, 4, 1]]
+    },
+    {
+        numRows: 1,
+        target: [[1]]
+    }
+]
+
+test('118. 杨辉三角', () => {
+    for (let i of example) {
+        const res = generate(i.numRows)
+
+        expect(res).toEqual(i.target)
+    }
+});
--- code/array/118/index.ts
+++ code/array/118/index.ts
@@ -1,3 +1,19 @@
-function generate(numRows: number): number[][] {
+/**
+ * @param numRows 
+ * @returns 
+ */
+export function generate(numRows: number): number[][] {
+    const result: number[][] = []
 
+    for (let i = 0; i < numRows; i++) {
+        const row = new Array(i + 1).fill(1)
+
+        for (let j = 1; j < row.length - 1; j++) {
+            row[j] = result[i - 1][j - 1] + result[i - 1][j]
+        }
+
+        result.push(row)
+    }
+
+    return result
 };
