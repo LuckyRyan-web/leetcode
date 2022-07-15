a35a93065209d98e734e3c5f4e57e7b33493744d
liuyuan <373005226@qq.com>
2022-07-15 22:44:52 +0800

    添加989题解

---
+++ code/array/989/index.test.ts
@@ -0,0 +1,29 @@
+/**
+ * @author liuyuan
+ * @date 2022-07-15 22:16
+ * @since 1.0.0
+ */
+
+const example_989: Types.ExampleType[] = [
+    {
+        num: [1, 2, 0, 0],
+        k: 34,
+        target: [1, 2, 3, 4]
+    },
+    {
+        num: [2, 7, 4],
+        k: 181,
+        target: [4, 5, 5]
+    },
+    {
+        num: [2, 1, 5],
+        k: 806,
+        target: [1, 0, 2, 1]
+    }
+]
+
+test('', () => {
+    example_989.forEach((v) => {
+
+    })
+})
---
+++ code/array/989/index.ts
@@ -0,0 +1,36 @@
+/**
+ * @author liuyuan
+ * @date 2022-07-15 22:16
+ * @since 1.0.0
+ */
+
+function addToArrayForm(num: number[], k: number): number[] {
+    const res: number[] = []
+
+    // 从数组的最右边开始计算，来模拟数字相加操作
+    const len = num.length
+
+    for (let i = len - 1; i >= 0; --i) {
+        let carry = num[i] + k % 10
+
+        k = Math.floor(k / 10)
+
+        // 两个个位数相加最大也就是 10，所以只需要考虑 >= 10 的情况下减掉 10
+        if (carry >= 10) {
+            k++
+            carry = carry - 10
+        }
+
+        res.push(carry)
+    }
+
+    // 如果 k 还是有数字的话，就是出现了 k 大于 num 数字的情况
+    while (k > 0) {
+        res.push(k % 10)
+        k = Math.floor(k / 10)
+    }
+
+    res.reverse()
+
+    return res
+};
