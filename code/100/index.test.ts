a826d91ccd4c935ed3e7349761a1e4d2022dc642
liuyuan <373005226@qq.com>
2022-07-28 11:40:56 +0800

    添加 100 题解

---
+++ code/100/index.test.ts
@@ -0,0 +1,13 @@
+/**
+ * @author liuyuan
+ * @date 2022-07-28 11:36
+ * @since 1.0.0
+ */
+
+const example_100:Types.ExampleType[] = []
+
+test('100', () => {
+    example_100.forEach((v) => {
+        
+    })
+})
---
+++ code/100/index.ts
@@ -0,0 +1,35 @@
+/**
+ * @author liuyuan
+ * @date 2022-07-28 11:36
+ * @since 1.0.0
+ */
+
+/**
+ * Definition for a binary tree node.
+ * class TreeNode {
+ *     val: number
+ *     left: TreeNode | null
+ *     right: TreeNode | null
+ *     constructor(val?: number, left?: TreeNode | null, right?: TreeNode | null) {
+ *         this.val = (val===undefined ? 0 : val)
+ *         this.left = (left===undefined ? null : left)
+ *         this.right = (right===undefined ? null : right)
+ *     }
+ * }
+ */
+
+/**
+ * 直接用深度优先遍历递归自身节点即可
+ */
+
+function isSameTree(p: TreeNode | null, q: TreeNode | null): boolean {
+    if (!p || !q) {
+        return !p && !q
+    }
+
+    return (
+        p.val === q.val &&
+        isSameTree(q.left, p.left) &&
+        isSameTree(q.right, p.right)
+    )
+}
