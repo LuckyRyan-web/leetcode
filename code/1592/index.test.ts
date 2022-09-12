d3483fa317d1bdf0c846f6e4376ecae98b775914
liuyuan <373005226@qq.com>
2022-09-12 23:35:33 +0800

    添加 1592 的题解

---
+++ code/1592/index.test.ts
@@ -0,0 +1,13 @@
+/**
+ * @author liuyuan
+ * @date 2022-09-09 11:44
+ * @since 1.0.0
+ */
+
+const example_1592:Types.ExampleType[] = []
+
+test('1592', () => {
+    example_1592.forEach((v) => {
+        
+    })
+})
---
+++ code/1592/index.ts
@@ -0,0 +1,57 @@
+/**
+ * leetcode: https://leetcode.cn/problems/rearrange-spaces-between-words/
+ * Tags: 字符串模拟
+ * @author liuyuan
+ * @date 2022-09-09 11:44
+ * @since 1.0.0
+ */
+
+function reorderSpaces(text: string): string {
+    const words: string[] = []
+
+    let result = ''
+
+    text.split(' ').forEach((v) => {
+        if (v) {
+            words.push(v)
+        }
+    })
+
+    let nullCount = 0
+
+    text.split('').forEach((v) => {
+        if (v === ' ') {
+            nullCount++
+        }
+    })
+
+    /** 空格字符串个数 */
+    const emptyLen = Math.floor(nullCount / (words.length - 1))
+
+    /** 余数个数 */
+    let remainder = 0
+
+    if (words.length > 1) {
+        remainder = nullCount % (words.length - 1)
+    } else if (words.length === 1) {
+        remainder = nullCount
+    } else {
+        remainder = 0
+    }
+
+    if (words.length <= 1) {
+        result = words[0]
+
+        if (remainder) {
+            result = result + ' '.repeat(remainder)
+        }
+    } else {
+        result = words.join(' '.repeat(emptyLen))
+
+        if (remainder) {
+            result = result + ' '.repeat(remainder)
+        }
+    }
+
+    return result
+}
